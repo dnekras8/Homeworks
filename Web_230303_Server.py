c246ddac053322569897e1dc7a499048b9f3d983
dnekras8 <denisnekrasov75@gmail.com>
2023-03-03 18:04:34 +0500

    Initial commit

---
+++ Web_230303_Server.py
@@ -0,0 +1,20 @@
+# Разработать простейший TCP echo сервер.
+#
+# Требования
+#
+# Запускается на IP адресе 0.0.0.0 и TCP порту 2222
+# Получает сообщения длинной не более 1024 байт и отправляет обратно клиенту
+# Закрывает соединение при получении сообщения с текстом close
+import socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('0.0.0.0', 2222))
+s.listen(10)
+while True:
+    conn, addr = s.accept()
+    while True:
+        data = conn.recv(1024)
+        if not data: break
+        conn.send(data)
+    #conn.close()
+    if data == 'close': conn.close()
+
